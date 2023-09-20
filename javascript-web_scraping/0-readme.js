bf48b6026c8c4fa3ebf7ca20659826ae5ba2aa5a
Frensi <user@Users-MacBook.local>
2023-09-20 12:52:37 +0200

    add

--- javascript-web_scraping/0-readme.js
+++ javascript-web_scraping/0-readme.js
@@ -1,11 +1,6 @@
 #!/usr/bin/node
-
 const fs = require('fs');
-
 fs.readFile(process.argv[2], 'utf8', (err, data) => {
-  if (err) {
-    console.error(err);
-    return;
-  }
+  if (err) throw err;
   console.log(data);
 });
