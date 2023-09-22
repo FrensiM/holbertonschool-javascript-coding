a49413fdb3a7df85f220b38987a26be861c00b72
FrensiM <frensimeta@gmail.com>
2023-09-22 15:32:10 +0000

    add

--- javascript-web_scraping/4-starwars_count.js
+++ javascript-web_scraping/4-starwars_count.js
@@ -1,19 +1,20 @@
 #!/usr/bin/node
+const process = require('process');
+const request = require('request');
+const api = process.argv[2];
 
-const axios = require('axios');
-let count = 0;
-
-axios.get(process.argv[2])
-  .then(function (response) {
-    response.data.results.forEach(element => {
-      element.characters.forEach(movies => {
-        if (movies.includes('18')) {
-          count++;
-        }
-      });
-    });
-    console.log(count);
-  })
-  .catch(function (error) {
-    console.log(error);
+request(api, function (error, response, body) {
+  if (error) {
+    console.error('error:', error); // Print the error if one occurred
+  }
+  const films = JSON.parse(response.body); // Print the response status code if a response was received
+  const filtered = films.results.filter(film => {
+    for (const character of film.characters) {
+      if (character.includes('18')) {
+        return true;
+      }
+    }
+    return false;
   });
+  console.log(filtered.length);
+});
